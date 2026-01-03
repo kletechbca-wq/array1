79279647781bff379e4bca0b0fd50c2d3200fb84
harshithashetty243-cmd <harshithashetty243@gmail.com>
2026-01-03 10:00:55 +0530

    modifed array

--- arr.py
+++ arr.py
@@ -6,5 +6,7 @@ def main():
     print(f"Scores: {scores}")
     print(f"Sum: {total}")
     print(f"Average: {average}")
+    print(f"minimum: {min(scores)}")
+    print(f"maximum: {max(scores)}")
 if __name__ == "__main__":
     main()
